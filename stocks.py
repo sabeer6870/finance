cbe432e6c05b0364440eeefb2f37474b1f407fc9
Mohammed <mohammed.sheikh@amadeus.com>
2017-10-22 15:34:38 -0400

    Drop I

--- stocks.py
+++ stocks.py
@@ -5,79 +5,66 @@ except ImportError:
     from BeautifulSoup import BeautifulSoup 
 
 import sys
-weekStart=sys.argv[1]
-weekEnd=sys.argv[2]
-day=sys.argv[3]
+
+# ex 2017-Oct-23
+targetDate=sys.argv[1]
 
 class WSJ:
     def getAverageAnalystPrice(self, symbol):
         url = 'http://quotes.wsj.com/' + symbol + '/research-ratings'
-        page = urllib2.urlopen('http://quotes.wsj.com/WMT/research-ratings')
+        page = urllib2.urlopen(url)
         soup = BeautifulSoup(page, "html.parser")
         elements = soup.findAll('div', {"data-module-id":"7"})
         if len(elements) == 0:
             print 'WSJ no average price found for stock' + symbol
             return
         x = elements[0].find_all('tr')
-        y = x[3].find_all('td')[1:]
-        y[0].get_text()
-        return y[0].get_text().strip().encode('ascii', 'ignore')
-
-class Yahoo:
-    def getStocksListNextSevenDays(self):
-        base = 'https://finance.yahoo.com/calendar/earnings?from=' + weekStart + '&to=' + weekEnd + '&day=' + day
-        symbolList = list()
-        sum = 0;
-        offset = 0
-        while offset < 400:
-            url = base + '&offset=' + str(offset) + '&size=100'
-            print url
-            page = urllib2.urlopen(base)
-            soup = BeautifulSoup(page, "html.parser")
-            elements = soup.findAll('div', {"data-reactid":"15"})
-            print len(elements)
-            if len(elements) < 3:
-                print 'Yahoo Earning returned list of size less than 2, i.e no earnings found'
-                break 
-            sum = sum + len(elements[2].find_all('tr'))
-            x = elements[2].find_all('tr')
-            i = 0
-            symbolList = list()
-            for row in x[0].find_all('tr'):
-                if i == 0:
-                    i += 1
-                    continue;
-                symbolList.append(row.a['data-symbol'].encode('ascii','ignore'))
-            offset = offset + 100
-        print sum    
-        return symbolList
+        avgPrice = x[3].find_all('td')[1:]
+        avgPrice[0].get_text()
+        currPrice = x[4].find_all('td')[1:]
+        currPrice[0].get_text()
+        return "Average price=" + avgPrice[0].get_text().strip().encode('ascii', 'ignore') + " current price= " + currPrice[0].get_text().strip().encode('ascii', 'ignore')
 
 class Zacks:
     def filterStocks(self, symbolList):
-        print symbolList
         base = 'https://www.zacks.com/stock/quote/'
         stockList = list()
+        wsjZ = WSJ()
         for symbol in symbolList:
             url = base + symbol
             page  = urllib2.urlopen(url)
             soup = BeautifulSoup(page, "html.parser")
             elements = soup.findAll('section', {"id":"premium_research"})
             researchRating = soup.findAll('div', {'class': 'callout_box3 pad10'})
-            if researchRating == 0:
-                print 'Rating not founf for stock ' + symbol
+            if len(researchRating) == 0:
+                print 'Rating not found for stock ' + symbol
                 continue
             row =  researchRating[0].find_all('tr')
+            if len(row) == 0:
+                print 'some problem ' + symbol
+                continue
             column = row[0].find_all('td')[0]
             rating = column.get_text().strip()
-            if rating == "Strong Buy 1":
-                stockList.append(symbol)
-            elif rating == "Buy 2":
-                stockList.append(symbol)
+            if rating == "Strong Buy 1" or rating == "Buy 2" :
+                stockList.append(symbol + " Zack Rank "+ rating + " Wsj Average price " + wsjZ.getAverageAnalystPrice(symbol))
         return stockList
 
-wsj = WSJ()
-wsj.getAverageAnalystPrice('WMT')
-yahoo = Yahoo()
-sList = yahoo.getStocksListNextSevenDays()
+class Nasdaq:
+    def findAllSymbol(self):
+        url = 'http://www.nasdaq.com/earnings/earnings-calendar.aspx?date=' + targetDate
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page, "html.parser")
+        elements = soup.findAll('table', {"id":"ECCompaniesTable"})
+        myList = list()
+        rows = elements[0].find_all('tr')
+        for x in range(1, len(rows)):
+            ref = rows[x].a['href'].encode('ascii','ignore')
+            myList.append(ref.split("/")[-2].upper())
+        return myList
+
+nasdaq = Nasdaq()
+oneDayList = nasdaq.findAllSymbol()
 zacks = Zacks()
-print zacks.filterStocks(['WMT', 'HUBS', 'MXIM', 'JRONY'])
+buyList = zacks.filterStocks(oneDayList)
+for s in buyList:
+    print s
