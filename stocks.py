b34b3b9b5c1a1ae9a9d8f336874a4bb809982542
Mohammed <mohammed.sheikh@amadeus.com>
2017-11-05 11:52:17 -0500

    Tabular Result

--- stocks.py
+++ stocks.py
@@ -9,18 +9,22 @@ import sys
 #python stocks.py 2017-Oct-24 > Oct24.log
 # ex 2017-Oct-23
 targetDate=sys.argv[1]
+columSeparator = ' | '
+Length = 15
+TotalWidth = 33
+Breaker =  "|"+"- " * TotalWidth + " |"
 
 class WSJ:
-    def isCurrentPriceMoreThanAvgPrice(self, symbol):
+    def ratingFromWsj(self, symbol):
         url = 'http://quotes.wsj.com/' + symbol + '/research-ratings'
         page = urllib2.urlopen(url)
         soup = BeautifulSoup(page, "html.parser")
         analystRatingTable = soup.findAll('table', {"class":"cr_dataTable"})
         if len(analystRatingTable) < 6:
-            return False, '', 0, 0, 0, 0, 0
+            return 'NA', '0-0-0-0-0', 0
         rows = analystRatingTable[5].find_all('tr')
         if len(rows) < 7:
-            return False, '', 0, 0, 0, 0, 0
+            return 'NA', '0-0-0-0-0', 0
         buyRating = rows[1].find_all('td')
         overWeightRating = rows[2].find_all('td')
         holdRating = rows[3].find_all('td')
@@ -28,39 +32,39 @@ class WSJ:
         sellRating = rows[5].find_all('td')
         overAllRating = rows[6].find_all('td')
         if len(buyRating) < 4 or len(overWeightRating)  < 4 or len(holdRating) < 4 or len(underweightRating) < 4 or len(sellRating) < 4:
-            return False, '', 0, 0, 0, 0, 0
+            return 'NA', '0-0-0-0-0', 0
         numberOfBuy = buyRating[3].findAll('span', {'class': "data_data"})
         numberOfOverweight = overWeightRating[3].findAll('span', {'class': "data_data"})
         numberOfHold = holdRating[3].findAll('span', {'class': "data_data"})
         numberOfUnderWeight = underweightRating[3].findAll('span', {'class': "data_data"})
         numberOfSell = sellRating[3].findAll('span', {'class': "data_data"})
         r = overAllRating[3].findAll('div', {'class': "numValue-content"})[0].text.strip()
-        if (r == 'Overweight' or r == 'Buy'):
-            return True, r, numberOfBuy[0].text.strip(), numberOfOverweight[0].text.strip(), numberOfHold[0].text.strip(), numberOfUnderWeight[0].text.strip(), numberOfSell[0].text.strip()
-        return False, r, numberOfBuy[0].text.strip(), numberOfOverweight[0].text.strip(), numberOfHold[0].text.strip(), numberOfUnderWeight[0].text.strip(), numberOfSell[0].text.strip()
+        summary = numberOfBuy[0].text.strip()+"-"+ numberOfOverweight[0].text.strip()+"-"+ numberOfHold[0].text.strip()+"-"+ numberOfUnderWeight[0].text.strip()+"-"+ numberOfSell[0].text.strip()
+        totalNumberOfRating = int(numberOfBuy[0].text.strip()) + int(numberOfOverweight[0].text.strip()) + int(numberOfHold[0].text.strip()) + int(numberOfUnderWeight[0].text.strip()) + int(numberOfSell[0].text.strip())
+        if (r == 'Overweight' or r == 'Buy' or r == 'Hold'):
+            return r, summary, totalNumberOfRating
+        return 'NA', summary, totalNumberOfRating
 
 class Zacks:
-    def filterStocks(self, symbolList):
+    def ratingFromZacks(self, symbol):
         base = 'https://www.zacks.com/stock/quote/'
-        stockList = list()
-        for symbol in symbolList:
-            url = base + symbol
-            page  = urllib2.urlopen(url)
-            soup = BeautifulSoup(page, "html.parser")
-            elements = soup.findAll('section', {"id":"premium_research"})
-            researchRating = soup.findAll('div', {'class': 'callout_box3 pad10'})
-            if len(researchRating) == 0:
-                print 'Rating not found for stock ' + symbol
-                continue
-            row =  researchRating[0].find_all('tr')
-            if len(row) == 0:
-                print 'some problem ' + symbol
-                continue
-            column = row[0].find_all('td')[0]
-            rating = column.get_text().strip()
-            if rating == "Strong Buy 1" or rating == "Buy 2" or rating == "Hold 3":
-                stockList.append(symbol + '-' + rating)
-        return stockList
+        url = base + symbol
+        page  = urllib2.urlopen(url)
+        soup = BeautifulSoup(page, "html.parser")
+        elements = soup.findAll('section', {"id":"premium_research"})
+        researchRating = soup.findAll('div', {'class': 'callout_box3 pad10'})
+        if len(researchRating) == 0:
+            print 'Rating not found for stock ' + symbol
+            return 'NA'
+        row =  researchRating[0].find_all('tr')
+        if len(row) == 0:
+            print 'some problem ' + symbol
+            return 'NA'
+        column = row[0].find_all('td')[0]
+        rating = column.get_text().strip()
+        if rating == 'Strong Buy 1' or rating == 'Buy 2' or rating == 'Hold 3':
+            return rating
+        return 'NA' 
 
 class Nasdaq:
     def findAllSymbol(self):
@@ -77,30 +81,25 @@ class Nasdaq:
 
 nasdaq = Nasdaq()
 oneDayList = nasdaq.findAllSymbol()
+print len(oneDayList)
+#oneDayList = ['FANG', 'COHR', 'RSPP']
 zacks = Zacks()
-buyList = zacks.filterStocks(oneDayList)
 wsj = WSJ()
-finalList = list()
-#buyList = ['ANET-Buy']
-for s in buyList:
-    sn = s.split("-")[0]
-    zr = s.split("-")[1]
-    overall = ''
-    b,r,nb,no,nh,nu,ns = wsj.isCurrentPriceMoreThanAvgPrice(sn)
-    if r == '' or zr == '':
-        continue
-    rt = (s.split("-")[1] == 'Hold 3'and r == 'Hold')
-    if b == True: #and rt == False:
-        if zr == 'Strong Buy 1':
-            overall  = overall + 'A' 
-        elif zr == 'Buy 2':
-            overall = overall + 'B'
-        elif zr == 'Hold 3':
-            overall = overall + 'C'
-        if r == 'Buy':
-            overall = overall + 'A'
-        elif r == 'Overweight':
-            overall = overall + 'B'
-        elif r == 'Hold':
-            overall = overall + 'C'
-        print s + '-' + r + " overall= " + overall + " data=" + nb + "-" + no + "-" + nh + "-" + nu + "-" + ns
+
+print Breaker
+print columSeparator.join(['|Symbol'.ljust(Length),'Zacks'.ljust(Length), 'WSJ'.ljust(Length), 'WSJ All Rating'.ljust(Length-1) + "|"])
+print Breaker
+for item in oneDayList:
+    singleList = list()
+    zRating = zacks.ratingFromZacks(item)
+    if zRating != 'NA':
+        wsjRating,summary, totalNumberOfRating = wsj.ratingFromWsj(item)
+        bothHold = (wsjRating == 'Hold' and zRating == 'Hold 3')
+        noneHold = (wsjRating != 'Hold' and zRating != 'Hold 3')
+        if wsjRating != 'NA' and totalNumberOfRating > 5 and bothHold == False:
+            singleList.append("|" + item.ljust(Length-1))
+            singleList.append(zRating.encode('ascii','ignore').ljust(Length))
+            singleList.append(wsjRating.encode('ascii','ignore').ljust(Length))
+            singleList.append(summary.encode('ascii','ignore').ljust(Length-1)+ "|")
+            print columSeparator.join(singleList)
+            print Breaker
